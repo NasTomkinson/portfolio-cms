29b57509bd701ed4445b50648b50075f43ff737d
NasTomkinson <nastomkinson@gmail.com>
2026-04-02 14:51:25 +0100

    feat: added s3 bucket

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,6 @@
 export default [
   'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
+  'strapi::errors', 
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +8,28 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            '*.s3.eu-west-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            '*.s3.eu-west-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
